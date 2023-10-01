1ed11cd85436d70d9eb1f3583075f51817403ed8
issa <iaa47@njit.edu>
2023-10-01 12:55:03 -0400

    adding template baselines

--- M2/Problem3.php
+++ M2/Problem3.php
@@ -14,7 +14,7 @@ function bePositive($arr) {
     }
     echo "<pre>" . var_export($arr, true) . "</pre>";
 }
-//note: use the $arr variable, don't directly touch $a1-$a4
+    //note: use the $arr variable, don't directly touch $a1-$a4
     //TODO use echo to output all of the values as positive (even if they were originally positive) and maintain the original datatype
     //hint: may want to use var_dump() or similar to show final data types
    
