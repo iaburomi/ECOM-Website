e1b1c8f9970cd884ba872afcba07877c077ba458
issa <iaa47@njit.edu>
2023-12-17 15:32:36 -0500

    hw 3

--- login.php
+++ login.php
@@ -1,5 +1,10 @@
 <?php
 require(__DIR__ . "/partials/nav.php");
+if (isset($_SESSION['logout_message'])) {
+    echo "<p>{$_SESSION['logout_message']}</p>";
+    // Remove the logout message from the session to prevent it from displaying again
+    unset($_SESSION['logout_message']);
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,7 +29,7 @@ require(__DIR__ . "/partials/nav.php");
         }
 
         main {
-            max-width: 800px;
+            max-width: 400px;
             margin: 20px auto;
             padding: 20px;
             background-color: #fff;
@@ -32,8 +37,30 @@ require(__DIR__ . "/partials/nav.php");
             border-radius: 8px;
         }
 
-        h1 {
-            color: #333;
+        form {
+            display: flex;
+            flex-direction: column;
+        }
+
+        label {
+            margin-bottom: 8px;
+        }
+
+        input {
+            padding: 8px;
+            margin-bottom: 16px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
+
+        input[type="submit"] {
+            background-color: #007bff;
+            color: #fff;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #0056b3;
         }
 
         p {
@@ -48,91 +75,96 @@ require(__DIR__ . "/partials/nav.php");
         a:hover {
             text-decoration: underline;
         }
+
+        .error {
+            color: #dc3545;
+            margin-top: 10px;
+        }
     </style>
 </head>
-
 <body>
 
-<header>
-    <h1>Login</h1>
-</header>
+    <header>
+        <h1>Login</h1>
+    </header>
 
-<main>
-    <form onsubmit="return validate(this)" method="POST">
-        <div>
-            <label for="email">Email</label>
-            <input type="email" name="email" required />
-        </div>
-        <div>
-            <label for="username">Username</label>
-            <input type="text" name="username" required />
-        </div>
-        <div>
-            <label for="password">Password</label>
-            <input type="password" id="pw" name="password" required minlength="8" />
-        </div>
-        <input type="submit" value="Login" />
-    </form>
+    <main>
+        <form onsubmit="return validate(this)" method="POST">
+            <div>
+                <label for="email">Email</label>
+                <input type="email" name="email" required />
+            </div>
+            <div>
+                <label for="username">Username</label>
+                <input type="text" name="username" required />
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" id="pw" name="password" required minlength="8" />
+            </div>
+            <input type="submit" value="Login" />
+        </form>
 
-    <?php
-    require_once(__DIR__ . "/lib/functions.php");
+        <?php
+        require_once(__DIR__ . "/lib/functions.php");
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $email = se($_POST, "email", "", false);
-        $username = se($_POST, "username", "", false);
-        $password = se($_POST, "password", "", false);
-        $hasError = false;
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $email = se($_POST, "email", "", false);
+            $username = se($_POST, "username", "", false);
+            $password = se($_POST, "password", "", false);
+            $hasError = false;
 
-        if (empty($email)) {
-            echo "<p class='error'>Email must not be empty</p>";
-            $hasError = true;
-        }
+            if (empty($email)) {
+                echo "<p class='error'>Email must not be empty</p>";
+                $hasError = true;
+            }
 
-        if (empty($username)) {
-            echo "<p class='error'>Username must not be empty</p>";
-            $hasError = true;
-        }
+            if (empty($username)) {
+                echo "<p class='error'>Username must not be empty</p>";
+                $hasError = true;
+            }
 
-        // Sanitize and validate email
-        $email = sanitize_email($email);
-        if (!is_valid_email($email)) {
-            echo "<p class='error'>Invalid email address</p>";
-            $hasError = true;
-        }
+            // Sanitize and validate email
+            $email = sanitize_email($email);
+            if (!is_valid_email($email)) {
+                echo "<p class='error'>Invalid email address</p>";
+                $hasError = true;
+            }
 
-        if (empty($password)) {
-            echo "<p class='error'>Password must not be empty</p>";
-            $hasError = true;
-        }
+            if (empty($password)) {
+                echo "<p class='error'>Password must not be empty</p>";
+                $hasError = true;
+            }
 
-        if (!$hasError) {
-            $db = getDB();
-            $stmt = $db->prepare("SELECT id, email, password FROM Users WHERE email = :email");
-            try {
-                $r = $stmt->execute([":email" => $email]);
-                if ($r) {
-                    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-                    if ($user && password_verify($password, $user["password"])) {
-                        session_start(); // Ensure session is started
-                        $_SESSION["user"] = [
-                            "id" => $user["id"],
-                            "email" => $user["email"],
-                        ];
-                        header("Location: home.php");
-                        exit();
-                    } else {
-                        echo "<p class='error'>Invalid email or password</p>";
+            if (!$hasError) {
+                $db = getDB();
+                $stmt = $db->prepare("SELECT id, email, password FROM Users WHERE email = :email");
+                try {
+                    $r = $stmt->execute([":email" => $email]);
+                    if ($r) {
+                        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+                        if ($user && password_verify($password, $user["password"])) {
+                            session_start(); // Ensure session is started
+                            $_SESSION["user"] = [
+                                "id" => $user["id"],
+                                "email" => $user["email"],
+                            ];
+                            header("Location: home.php");
+                            exit();
+                        } else {
+                            echo "<p class='error'>Invalid email or password</p>";
+                        }
                     }
+                } catch (Exception $e) {
+                    echo "<p class='error'>There was an error processing your request</p>";
+                    echo "<pre>" . var_export($e, true) . "</pre>";
                 }
-            } catch (Exception $e) {
-                echo "<p class='error'>There was an error processing your request</p>";
-                echo "<pre>" . var_export($e, true) . "</pre>";
             }
         }
-    }
-    ?>
+        ?>
 
-</main>
+    </main>
 
 </body>
+
 </html>
--- logout.php
+++ logout.php
@@ -1,11 +1,16 @@
 <?php
 session_start(); // Ensure session is started
+
 // Store a logout message in a session variable
 $_SESSION['logout_message'] = "You have successfully logged out.";
-// Unset all session variables
-session_unset();
+
 // Destroy the session
+session_unset();
 session_destroy();
+
+// Ensure that the cookie is completely removed
+setcookie(session_name(), "", time() - 3600, "/");
+
 // Redirect to the login page
 header("Location: login.php");
 exit();
